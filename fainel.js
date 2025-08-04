6bb674266a8daadd775c98bff03d7f8eea1d4b93
deluwarhoisn <babuhossen301@gmail.com>
2025-08-04 22:21:15 +0600

    problem-4

--- fainel.js
+++ fainel.js
@@ -5,3 +5,27 @@ var area = 800;
 var value = area / 2;
 console.log(value);
 
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+
+if(money >= 25000){
+    console.log('Laptop')
+}
+else if(money>= 10000){
+    console.log('Cycle')
+}
+else{
+    console.log('Chocolate')
+}
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 11;
+
+for(var day= 1; day<=lastDay; day++){
+    if(day % 3 === 0) {
+        console.log(day+'- medicine');
+    }
+    else{
+        console.log(day+'- rest');
+    }
+}
