0f8b9a21bc1230f42877150c92791f9a099dd45c
deluwarhoisn <babuhossen301@gmail.com>
2025-08-04 22:24:22 +0600

    fainel

--- fainel.js
+++ fainel.js
@@ -48,3 +48,20 @@ var student= { name: "jhanku" , roll: 1014 ,department: "cse" };
 
 var Email = student.name + student.roll + '.' + student.department + '@ph.ac.bd';
 console.log(Email);
+
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+
+var incrementRate = 5;
+var currentSalary = startingSalary;
+for(var i =0; i < experience; i++){
+    currentSalary = currentSalary * (1 + incrementRate / 100);
+}
+console.log(currentSalary.toFixed(2));
+
+
+
+
+
+   
