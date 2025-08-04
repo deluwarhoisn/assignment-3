3980d1de1bbc592d9cb6ac602e6f08aa9ac15be0
deluwarhoisn <babuhossen301@gmail.com>
2025-08-04 22:23:09 +0600

    problem-5

--- fainel.js
+++ fainel.js
@@ -42,3 +42,9 @@ else{
     output = 'Delete';
 }
 console.log(output);
+
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhanku" , roll: 1014 ,department: "cse" };
+
+var Email = student.name + student.roll + '.' + student.department + '@ph.ac.bd';
+console.log(Email);
---
+++ problem-5.js
@@ -0,0 +1,5 @@
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhanku" , roll: 1014 ,department: "cse" };
+
+var Email = student.name + student.roll + '.' + student.department + '@ph.ac.bd';
+console.log(Email);
